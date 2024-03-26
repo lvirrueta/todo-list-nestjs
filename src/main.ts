8b44398579c1c6d523fd2c8a97cf141696d45462
lvirrueta <luis510vg@gmail.com>
2024-03-26 17:21:25 -0600

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('To Do List')
+    .setDescription('App To Do List')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    //.addSecurity('basic', {type: 'http',scheme: 'basic'})
+    //.addOAuth2()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      filter: true,
+      persistAuthorization: true,
+    },
+  });
+
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //   }),
+  // );
+
   await app.listen(3000);
 }
 bootstrap();
